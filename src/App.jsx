089a5d1bcbc71c153fa14e02cee302b84b7fdc67
Sudhir <sudhir.rai01@gmail.com>
2025-07-14 13:15:33 +0530

    added name

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Profile from "./sections/Profile.jsx";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Profile />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/sections/Profile.jsx
@@ -0,0 +1,29 @@
+import { Element, Link as LinkScroll } from "react-scroll";
+
+const Profile = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+          
+            <h1 className="h1 text-p4">
+              Sudhir
+            </h1>
+          
+          </div>
+
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              className="size-1230 max-lg:h-auto"
+              alt="hero"
+            />
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Profile;
